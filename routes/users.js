834513c3cef3d4b6711f21d53f757e70f916f13a
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-12-11 00:20:59 +0300

    Update user auth

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,8 @@ router.post("/signin", async (req, res) => {
         .status(404)
         .json({ error: "User with this email doesn't exist" });
     }
-    if (!bcrypt.compare(password, user.password)) {
+    const samePassword = await bcrypt.compare(password, user.password)
+    if (!samePassword) {
       return res.status(400).json({ error: "Wrong password" });
     }
     const token = jwt.sign({ id: user._id }, process.env.SECRET, {
