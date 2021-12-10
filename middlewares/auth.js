3953a7e11e0916128224f29a9ab3c4d69f431a92
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-12-11 00:22:09 +0300

    Create auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async function (req, res, next) {
+  try {
+    if (req.method === "OPTIONS") return next();
+
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) throw new Error("No token provided");
+    const data = jwt.verify(token, process.env.SECRET);
+    req.decoded = data;
+    return next();
+  } catch (error) {
+    console.log(error.message);
+    return res.status(401).send({
+      error: "Not Authenticated",
+    });
+  }
+};
